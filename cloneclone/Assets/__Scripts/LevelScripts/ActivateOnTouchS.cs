6c36069b667db63751c5c088d1be3e4009029599
chorgan93 <chorgan93@gmail.com>
2016-07-26 00:22:25 -0700

    level design
    me

---
+++ cloneclone/Assets/__Scripts/LevelScripts/ActivateOnTouchS.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ActivateOnTouchS : MonoBehaviour {
+
+	public List<GameObject> turnOnObjects;
+	public List<GameObject> turnOffObjects;
+
+	private bool turnedOn = false;
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "Player" && !turnedOn){
+
+			foreach (GameObject eh in turnOnObjects){
+				eh.SetActive(true);
+			}
+			foreach (GameObject bleh in turnOffObjects){
+				bleh.SetActive(false);
+			}
+
+			turnedOn = false;
+		}
+	}
+}
