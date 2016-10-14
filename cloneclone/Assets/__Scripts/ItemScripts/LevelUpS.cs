2b32d872992e534f5abb74deb6dc9357c8ec0d60
chorgan93 <chorgan93@gmail.com>
2016-10-13 20:57:55 -0700

    first steps
    to something greater

---
+++ cloneclone/Assets/__Scripts/ItemScripts/LevelUpS.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelUpS : MonoBehaviour {
+
+	public int upgradeID = -1; // determines type of upgrade (health, stamina, charge, etc)
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ cloneclone/Assets/__Scripts/UIScripts/LevelUpItemS.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LevelUpItemS : MonoBehaviour {
+	
+	private PlayerInventoryS inventoryRef;
+
+	// TODO: Make this VVV
+	//private LevelUpS upgradeRef;
+
+	public Image upgradeImage;
+	public int upgradeNum;
+
+	public string upgradeDescription;
+	private bool _unlocked = false;
+	public bool unlocked { get { return _unlocked; } }
+
+	public void Initialize(PlayerInventoryS i){
+
+		inventoryRef = i;
+
+		bool turnOn = false;
+		/*foreach (PlayerWeaponS w in i.unlockedWeapons){
+			if (w.weaponNum == weaponNum){
+				turnOn = true;
+				weaponRef = w;
+			}
+		}
+
+		if (!turnOn){
+			weaponImage.enabled = false;
+			//weaponName.enabled = false;
+			_unlocked = false;
+		}else{
+			weaponImage.sprite = weaponRef.swapSprite;
+			weaponImage.color = weaponRef.swapColor;
+			weaponImage.enabled = true;
+
+			//weaponName.color = weaponRef.swapColor;
+			//weaponName.enabled = true;
+			_unlocked = true;
+		}**/
+
+	}
+
+	//public PlayerWeaponS WeaponRefForSwitch(){
+		//return weaponRef;
+	//}
+}
