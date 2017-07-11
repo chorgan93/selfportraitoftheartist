99dee418358e1b7951edbf7c781d1d28d002418f
chorgan93 <chorgan93@gmail.com>
2017-07-11 15:22:15 -0700

    I THINK I DID IT
    DO SOMETHING ELSE THO

--- cloneclone/Assets/__Scripts/EnemyScripts/EnemyBehaviors/EnemyLogicBehaviors/EnemyReorderBehaviorS.cs
+++ cloneclone/Assets/__Scripts/EnemyScripts/EnemyBehaviors/EnemyLogicBehaviors/EnemyReorderBehaviorS.cs
@@ -9,10 +9,14 @@ public class EnemyReorderBehaviorS : EnemyBehaviorS {
 	public override void StartAction(bool setAnimTrigger=true){
 
 		behaviorToExecute = possBehaviorSteps[Mathf.FloorToInt(Random.Range(0, possBehaviorSteps.Length))];
-		base.EndAction(false);
+		base.CancelAction();
 		myEnemyReference.currentState.behaviorSet[behaviorToExecute].SetEnemy(myEnemyReference);
 		myEnemyReference.currentState.behaviorSet[behaviorToExecute].StartAction();
 		myEnemyReference.currentState.SetActingBehaviorNum(behaviorToExecute);
 
+	}
+
+	public override void EndAction(bool dontMatter = false){
+		base.EndAction(false);
 	}
 }
