5ce5567768b34a4bbaf22636e06f00569d2e0c21
chorgan93 <chorgan93@gmail.com>
2017-10-13 13:07:05 -0700

    cloud
    woop

---
+++ cloneclone/Assets/__Scripts/LevelScripts/SuburbScripts/CloudS.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudS : MonoBehaviour {
+
+	public float driftSpeed;
+	private float currentDriftSpeed;
+	public float speedVariation;
+	public Vector3 driftDirection;
+	private Color currentCol;
+	public float fadeRate = 0.25f;
+	private Vector3 startPos;
+	private Vector3 currentPos;
+	public float yPositionVariation;
+	public float lifeTime = 10f;
+	private float currentLifeTime;
+	public bool fadingIn = false;
+	public bool fadingOut = false;
+	private float maxA;
+
+	private SpriteRenderer myRenderer;
+
+	// Use this for initialization
+	void Start () {
+
+		transform.parent =null;
+		myRenderer = GetComponent<SpriteRenderer>();
+		maxA = myRenderer.color.a;
+		if (fadingIn){
+			currentCol = myRenderer.color;
+			currentCol.a = 0f;
+			myRenderer.color = currentCol;
+		}
+		currentDriftSpeed = driftSpeed+speedVariation*Random.Range(-1f, -0.1f);
+		startPos = currentPos = transform.position;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (fadingIn){
+			currentCol = myRenderer.color;
+			currentCol.a += fadeRate*Time.deltaTime;
+			if (currentCol.a >= maxA){
+				currentCol.a = maxA;
+				fadingIn = false;
+			}
+			myRenderer.color = currentCol; 
+		}else if (fadingOut){
+
+			currentCol = myRenderer.color;
+			currentCol.a -= fadeRate*Time.deltaTime;
+			if (currentCol.a <= 0){
+				currentCol.a = 0;
+				myRenderer.color = currentCol;
+				fadingOut = false;
+				ResetCloud();
+			}
+			myRenderer.color = currentCol; 
+		}
+		Drift();
+	
+	}
+
+	void ResetCloud(){
+		currentPos = startPos;
+		currentPos.y += yPositionVariation*Random.insideUnitCircle.x;
+		transform.position = currentPos;
+		fadingIn = true;
+		currentLifeTime = lifeTime;
+		currentDriftSpeed = driftSpeed+speedVariation*Random.Range(-1f, -0.1f);
+	}
+
+	void Drift(){
+		currentLifeTime -= Time.deltaTime;
+		if (currentLifeTime <= 0 && !fadingOut){
+			fadingOut = true;
+		}
+		currentPos = transform.position;
+		currentPos += driftDirection*currentDriftSpeed*Time.deltaTime;
+		transform.position = currentPos;
+	}
+}
