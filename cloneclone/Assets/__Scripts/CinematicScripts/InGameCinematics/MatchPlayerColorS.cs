485fbd92a5bf09a79a2496d851e23ec19b95564a
chorgan93 <chorgan93@gmail.com>
2018-02-20 00:24:32 -0800

    christian 2 scene
    get that ai done and we set

---
+++ cloneclone/Assets/__Scripts/CinematicScripts/InGameCinematics/MatchPlayerColorS.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MatchPlayerColorS : MonoBehaviour {
+
+	public PlayerController playerRef;
+	public SpriteRenderer targetSprite;
+
+	// Use this for initialization
+	void Awake () {
+		targetSprite.color = playerRef.EquippedWeapon().swapColor;
+	}
+	
+
+}
---
+++ cloneclone/Assets/__Scripts/CinematicScripts/InGameCinematics/MovePlayerS.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlayerS : MonoBehaviour {
+
+	public PlayerController playerRef;
+	public Transform targetTransform;
+
+	// Use this for initialization
+	void Start () {
+		playerRef.transform.position = targetTransform.position;
+	}
+	
+
+}
