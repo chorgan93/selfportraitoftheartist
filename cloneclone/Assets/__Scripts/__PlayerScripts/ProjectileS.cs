a37881a4c53ae0d6f97c7d239a03123cd1a5a770
chorgan93 <chorgan93@gmail.com>
2018-02-28 17:59:20 -0800

    ay
    yaaaaay

--- cloneclone/Assets/__Scripts/__PlayerScripts/ProjectileS.cs
+++ cloneclone/Assets/__Scripts/__PlayerScripts/ProjectileS.cs
@@ -259,6 +259,7 @@ public class ProjectileS : MonoBehaviour {
 			dmg = startDmg;
 			transform.localScale = startScale;
 		}
+		_canReflect = _myPlayer.playerAug.repellantAug;
 		stopAtWallTime = false;
 		touchingWall = false;
 		enemiesHit.Clear();
